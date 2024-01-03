a38877c2745e64623251bb4dd6a95d732eded471
Kristoffer Tonning <tonning@gmail.com>
2024-01-03 12:08:29 +0100

    Add GetAttachmentsBySourceRequest

---
+++ src/Requests/Issues/GetAttachmentsBySourceRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Tonning\Linear\Requests\Issues;
+
+use Saloon\Contracts\Body\HasBody;
+use Tonning\Linear\Requests\Request;
+
+class GetAttachmentsBySourceRequest extends Request implements HasBody
+{
+    public function __construct(
+        public readonly string $issueId,
+        public readonly string $sourceType,
+    )
+    {
+        //
+    }
+
+    public function defaultBody(): array
+    {
+        $query = <<<QUERY
+            query Issue(\$issueId: String!, \$attachmentsFilter: AttachmentFilter) {
+              issue(id: \$issueId) {
+                attachments(filter: \$attachmentsFilter) {
+                  nodes { $this->returnNodes }
+                }
+              }
+            }
+            QUERY;
+
+        \Log::debug([
+            'query' => $query,
+            'variables' => [
+                'issueId' => $this->issueId,
+                'filter' => [
+                    'sourceType' => [
+                        'eq' => $this->sourceType
+                    ]
+                ],
+            ]
+        ]);
+
+        return [
+            'query' => $query,
+            'variables' => [
+                'issueId' => $this->issueId,
+                'filter' => [
+                    'sourceType' => [
+                        'eq' => $this->sourceType
+                    ]
+                ],
+            ]
+        ];
+    }
+}
